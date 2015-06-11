7033f1f9affee4279a37d4b33428d6795084d8dc
gatieme <gatieme@163.com>
2015-06-11 16:59:59 +0800

    创建了最初的目录结构，以及客户端和服务器的框架...

---
+++ server/DBTools.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace server
+{
+    class DBTools
+    {
+        #region  建立MySql数据库连接
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        /// <returns>返回MySqlConnection对象</returns>
+        public MySqlConnection getmysqlcon()
+        {
+            string M_str_sqlcon = "server=localhost;user id=root;password=root;database=signature"; //根据自己的设置
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            return myCon;
+        }
+        #endregion
+
+        #region  执行MySqlCommand命令
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public void getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();///  先获取一个MySql的
+            try
+            {
+                mysqlcon.Open();
+                MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+                mysqlcom.ExecuteNonQuery();
+                mysqlcom.Dispose();
+                mysqlcon.Close();
+                mysqlcon.Dispose();
+            }
+            catch
+            {
+                mysqlcon.Close();
+            }
+        }
+        #endregion
+
+        #region  创建MySqlDataReader对象
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            try
+            {
+                mysqlcon.Open();
+                //MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+                MySqlDataReader mysqlread = mysqlcom.ExecuteReader();
+                return mysqlread;
+        
+            }
+            catch
+            {
+                mysqlcon.Close();
+                return null;
+            }
+   
+            /*if(mysqlread.Read())   // 一次读一条记录
+            {
+                if(mysqlread["Name"].ToString()==textBox1.Text&&mysqlread["PassWord"].ToString()==textBox2.Text)
+                {
+                    frmJxkh myJxkh = new frmJxkh();
+                    myJxkh.Show();
+                }
+            }*/
+        }
+        #endregion
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using MySql.Data.MySqlClient;
+using Office;
+using Microsoft.Office.Interop.Word;
+using Word = Microsoft.Office.Interop.Word;  
+
+
+/*
+ * SignPress程序的服务器程序
+ * 
+ 
+ */
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SignPress服务器程序");
+            
+            /// 测试连接服务器以及查询测试
+            /**
+            DBTools dbtools = new DBTools();
+            MySqlDataReader mysqlread = dbtools.getmysqlread("SELECT * FROM department;");
+            while (mysqlread.Read( ))   // 一次读一条记录
+            {
+                Console.WriteLine(mysqlread["id"].ToString( ) + "  " + mysqlread["name"].ToString( ));
+            }**/
+
+            #region 第六行显示
+            //Start Word and create a new document.  
+            /*
+             * 首先添加COM组件Office，
+             * 然后添加.Net组件引用Microsoft.Office.Interop.Word;  
+             * 接着添加如下的代码
+             * using Office；
+             * using Microsoft.Office.Interop.Word;
+             * using Word = Microsoft.Office.Interop.Word;  
+             */
+            /// 对word的操作信息
+            object oMissing = System.Reflection.Missing.Value;  
+            object oEndOfDoc = "//endofdoc"; /* /endofdoc is a predefined bookmark */
+            object filename = @".\拨款会签单--航道局.doc";
+            //Start Word and create a new document.  
+            Word.Application oWord = new Word.Application();
+            oWord.Visible = true;  
+            // Word.Document oDoc = oWord.Documents.Add(ref oMissing, ref oMissing, ref oMissing, ref oMissing);              
+            Word.Documents oDoc = oWord.Documents.Open(
+
+            
+            #endregion
+        }
+    }
+}
